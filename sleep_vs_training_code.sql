fd1bfb58dd7250491e174c4fe7cc9e7fe9550c9d
Tereza Sásková <153806651+tercasaskova311@users.noreply.github.com>
2024-05-31 11:05:51 +0200

    Update sleep_vs_training_code.sql
    Signed-off-by: Tereza Sásková <153806651+tercasaskova311@users.noreply.github.com>

--- sleep_vs_training_code.sql
+++ sleep_vs_training_code.sql
@@ -6,32 +6,29 @@ FROM new_schema1.training_data ;
 
 SELECT Date 
 FROM new_schema1.sleep_data;
--- Select statement to display the formatted date
-SELECT
-    DATE_FORMAT(STR_TO_DATE(Date, '%d.%m.%Y'), '%Y-%d-%m') AS formatted_date
-FROM
-   new_schema1.sleep_data;
 
--- Update statement to change the date format in the table
+#Select statement to display the formatted date
+
+SELECT DATE_FORMAT(STR_TO_DATE(Date, '%d.%m.%Y'), '%Y-%d-%m') AS formatted_date
+FROM new_schema1.sleep_data;
+
+#Update statement to change the date format in the table
 SET SQL_SAFE_UPDATES = 0;
 
 UPDATE new_schema1.sleep_data
 SET Date = DATE_FORMAT(STR_TO_DATE(Date, '%d.%m.%Y'),'%d-%m-%Y');
 
-SELECT
-    DATE_FORMAT(STR_TO_DATE(WorkoutDay, '%Y-%m-%d'),'%d-%m-%Y') AS formatted_date
-FROM
-   new_schema1.training_data;
+SELECT DATE_FORMAT(STR_TO_DATE(WorkoutDay, '%Y-%m-%d'),'%d-%m-%Y') AS formatted_date
+FROM new_schema1.training_data;
    
 UPDATE new_schema1.training_data
 SET WorkoutDay = DATE_FORMAT(STR_TO_DATE(WorkoutDay, '%Y-%m-%d'),'%d-%m-%Y');
 
 
 #join training data and sleep to compare hours on bike in relation to sleep.
-SELECT
-WorkoutDay, Date  AS date
-FROM
-    new_schema1.training_data t1, new_schema1.sleep_data
+
+SELECT WorkoutDay, Date  AS date
+FROM new_schema1.training_data t1, new_schema1.sleep_data
 LEFT JOIN
     new_schema1.sleep_data t2 ON t1.WorkoutDay = t2.Date;
     
@@ -48,10 +45,8 @@ CREATE TABLE new_schema1.sleep_vs_training AS
 		t1.HRZone4,
 		t1.HRZone5,
 		t1.TSS
-FROM
-    new_schema1.training_data t1
-LEFT JOIN
-    new_schema1.sleep_data t2 ON t1.WorkoutDay = t2.Date;
+FROM new_schema1.training_data t1
+LEFT JOIN new_schema1.sleep_data t2 ON t1.WorkoutDay = t2.Date;
     
 SELECT * FROM new_schema1.sleep_vs_training 
 
@@ -74,8 +69,7 @@ DELETE FROM new_schema1.sleep_vs_training
 WHERE Time = '00:00:00';
 
 
-#duplicates
--- Step 1: Create a temporary table to store the IDs of the duplicate rows
+#duplicates - Create a temporary table to store the IDs of the duplicate rows
 CREATE TEMPORARY TABLE temp_duplicates AS
 SELECT Date
 FROM new_schema1.sleep_vs_training 
@@ -93,11 +87,11 @@ AND Date NOT IN (
 );
 SELECT * FROM temp_duplicates;
 
--- Step 2: Update the duplicate cells to NULL
+#Update the duplicate cells to NULL
 UPDATE my_table
 SET date = NULL
 WHERE id IN (SELECT id FROM temp_duplicates);
 
--- Step 3: Drop the temporary table
+#Drop the temporary table
 DROP TEMPORARY TABLE temp_duplicates;
 
